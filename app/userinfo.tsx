4f0c2d0acff847a201b3653d91c0aa3050a6f81e
ts336 <22297@apps.eggs.school.nz>
2026-08-12 14:15:54 +1200

    0.5.2: Adding inputs for name and age

--- app/userinfo.tsx
+++ app/userinfo.tsx
@@ -1,8 +1,11 @@
 import { router } from "expo-router";
-import React from "react";
-import { Pressable, ScrollView, Text } from "react-native";
+import { useState } from "react";
+import { Pressable, ScrollView, Text, TextInput } from "react-native";
 
 export default function UserInfo() {
+    const [name, onChangeName] = useState('');
+    const [age, onChangeAge] = useState('');
+
     return (
         <ScrollView
         contentContainerStyle={{
@@ -10,7 +13,20 @@ export default function UserInfo() {
             padding: 16,
         }}
         >
-            <Text>Hello</Text>
+            <Text>Welcome to Basic Pokedex! Enter the following to get started!</Text>
+            <TextInput
+                //style={}
+                placeholder="What is your name?"
+                onChangeText={onChangeName}
+                value={name}
+            />
+            <TextInput
+                //style={}
+                placeholder="What is your age?"
+                onChangeText={onChangeAge}
+                value={age}
+                keyboardType="numeric"
+            />
 
         <Pressable
         onPress={() => router.back()}

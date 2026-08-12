59648f8fff39b4db4dac73f3bc1dcdf402b464d3
ts336 <22297@apps.eggs.school.nz>
2026-08-12 13:15:45 +1200

    v0.3.0: Adding basic functionality

---
+++ app/details.tsx
@@ -0,0 +1,88 @@
+import { Stack } from "expo-router";
+import { useLocalSearchParams } from "expo-router/build/hooks";
+import React, { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text } from "react-native";
+
+type AbilityInfo = {
+    name: string;
+    description: string;
+
+}
+
+export default function Details() {
+    const params = useLocalSearchParams();
+    const [abilities, setAbilities] = useState<AbilityInfo[]>([]);
+
+    async function fetchPokemonByName(url: any) {
+        try {  
+            
+            const response = await fetch(url);
+            const details = await response.json();
+
+
+            const abilityInfo = await Promise.all(
+                details.abilities.map(async (item: any) => {
+                    const res = await fetch(item.ability.url);
+                    const abilityData = await res.json();
+
+
+                    const englishEntry = abilityData.effect_entries.find(
+                        (entry: any) => entry.language.name === "en"
+                    );
+
+                    return {
+                        name: item.ability.name,
+                        // ? only read short_effect if englishEntry exists
+                        // ?? otherwise use fallback string
+                        description: englishEntry?.short_effect ?? "No description found"
+                    };
+                })
+            );
+
+            setAbilities(abilityInfo);
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    useEffect(() => {
+        
+        fetchPokemonByName(params.url); // fetch selected pokemon details
+
+    }, [params.url]) // good practice: re-fetch whenever URL changes
+
+    return (
+        // <> </> (fragment) because component can only return one top-level JSX element
+        // <Stack.Screen> and <ScrollView> are siblings and thus need to be wrapped in a single parent
+        // satisfies the rule, does not add extra ui, better than View wrap when no styling/layout needed
+        <> 
+            <Stack.Screen options={{ title: params.name as string }}/>
+            <ScrollView
+            contentContainerStyle={{
+                gap: 16,
+                padding: 16,
+            }}
+            >
+                {abilities.map((ability) => (
+                    <React.Fragment key={ability.name}>
+                        <Text style={styles.ability}>{ability.name}</Text>
+                        <Text style={styles.description}>{ability.description}</Text>
+                    </React.Fragment>
+                    ))}
+            </ScrollView>
+        </>
+  );
+}
+
+const styles = StyleSheet.create({
+  ability: {
+    fontSize: 20,
+    textAlign: "center",
+  },
+  description: {
+    fontSize: 15,
+    textAlign: "center",
+    color: "grey",
+  }
+})
---
+++ app/index.tsx
@@ -0,0 +1,146 @@
+import { Link } from "expo-router";
+import { useEffect, useState } from "react";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+
+interface Pokemon {
+  name: string;
+  url: string;
+  image: string;
+  imageBack: string;
+  types: PokemonType[]; // array bc there can be more than one type
+}
+
+interface PokemonType {
+  type: {
+      name: string;
+      url: string;
+  }
+}
+
+const colorsByType = {
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  electric: "#F7D02C",
+  grass: "#7AC74C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD",
+}
+
+export default function Index() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  
+
+  useEffect(() => {
+    // fetch pokemon from api
+
+    fetchPokemons();
+  }, [])
+
+  async function fetchPokemons() {
+    try {
+      /* fetch is a function that lets u hit an API,
+      takes URL (endpoint) as a parameter and some request info
+      endpoint gives 20 pokemon in JSON format*/
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/?limit=20"
+      );
+      
+      /* pokemon saved in response variable 
+      but we need to convert it to JSON format 
+      so it can be used in app
+      */
+      const data = await response.json();
+
+
+      // fetch detailed info for each pokemon in parallel      
+      const detailedPokemons = await Promise.all(
+        data.results.map(async (pokemon: any) => {
+          const res = await fetch(pokemon.url);
+          const details = await res.json();
+          return {
+            name: pokemon.name,
+            image: details.sprites.front_default, // get image 
+            imageBack: details.sprites.back_default,
+            types: details.types,
+            url: pokemon.url,
+          };
+        })
+      );
+
+      // set state to detailedPokemons
+      setPokemons(detailedPokemons);
+
+    } catch (e) { // catch errors and log to console
+        console.log(e);
+    }
+  }
+
+  return (
+    <ScrollView
+      contentContainerStyle={{
+        gap: 16,
+        padding: 16,
+      }}
+    >
+      {pokemons.map((pokemon) => (
+        <Link 
+          key={pokemon.name}
+          href={{
+            pathname: "/details",
+            params: { name: pokemon.name, url: pokemon.url },
+          }}
+          style={{
+            // @ts-ignore this is happening because the types were not defined but not needed
+            backgroundColor: colorsByType[pokemon.types[0].type.name] + 50,
+            padding: 20,
+            borderRadius: 15,
+          }}
+        >
+          <View>
+              <Text style={styles.name}>{pokemon.name}</Text>
+              <Text style={styles.type}>{pokemon.types[0].type.name}</Text>
+              
+              <View style={{
+                flexDirection: "row",
+              }}>
+                <Image 
+                  source={{uri: pokemon.image}}
+                  style={{ width: 150, height: 150}}
+                />
+                <Image 
+                  source={{uri: pokemon.imageBack}}
+                  style={{ width: 150, height: 150}}
+                />
+              </View>
+          </View>
+        </Link>
+      ))}
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  name: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: "center",
+  },
+  type: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'grey',
+    textAlign: "center",
+  },
+})
